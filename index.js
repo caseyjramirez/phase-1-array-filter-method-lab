4968cadea461cbe8cf208c4dfdc15943c3582e05
caseyjramirez <caseyjramirez5@gmail.com>
2022-08-12 11:32:36 -0500

    commit tree a647a21128031a4c0a07bd039baf9e33d0ec8c47

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(e => e.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, string) {
+    const stringLength = string.length
+    return array.filter(e => e.substr(0, stringLength).toLowerCase() === string.toLowerCase())
+}
+
+function matchName(obj, string) {
+    const result = []
+    for (let e of obj) {
+        if(e.name === string) {
+            result.push(e)
+        }
+    }
+    return result
+}
